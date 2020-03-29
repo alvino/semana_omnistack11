180095ed8180061f8b148e2cf44e1b819bfc4fdf
alvino figueiredo <crazyduckalvino@gmail.com>
2020-03-29 01:14:46 -0300

    mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,29 @@
-import { useKeepAwake } from 'expo-keep-awake';
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
+
+import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake';
 import React from 'react';
 
 import Router from './src/routes'
 
-export default function App() {
-  useKeepAwake()
-  return (
-    <Router />
-  );
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router />
+    );
+  }
+
+  _activate = () => {
+    activateKeepAwake();
+    };
+
+  _deactivate = () => {
+    deactivateKeepAwake();
+    };
 }
+
 
